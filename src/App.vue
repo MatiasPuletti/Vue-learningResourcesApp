2287f59599a8cf9a1ddbf7dc74503c76afd2c8ba
matiaspuletti <pulettimatias@gmail.com>
2021-08-12 08:16:01 +0200

    Add BaseButton component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-<the-header title="RememberMe"></the-header>
-<stored-resources :resources="storedResources"></stored-resources>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
-import TheHeader from './components/layouts/TheHeader.vue'
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
     StoredResources,
-    TheHeader
+    TheHeader,
   },
   data() {
     return {
@@ -18,18 +18,18 @@ export default {
         {
           id: 'official-guide',
           title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
         },
         {
           id: 'google',
           title: 'Google',
-          description: 'Learn to Google',
-          link: 'https://google.com'
-        }
-      ]
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :type="type" :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #2e8bc0;
+  border: 1px solid #2e8bc0;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #0c2d48;
+  border-color: #0c2d48;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
