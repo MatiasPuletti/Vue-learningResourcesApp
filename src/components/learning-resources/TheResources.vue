6e306ffa3c72f3c93dd3fe7ce7b1973eeafbdaed
matiaspuletti <pulettimatias@gmail.com>
2021-08-14 23:12:13 +0200

    Fetch user input from the form and add it to the list of stored resources

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -3,15 +3,12 @@
     <base-button
       @click="setSelectedTab('stored-resources')"
       :mode="storedResButtonMode"
-      >Stored Resources</base-button
-    >
-    <base-button
-      @click="setSelectedTab('add-resource')"
-      :mode="addResButtonMode"
-      >Add Resource</base-button
-    >
+    >Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
-  <component :is="selectedTab"> </component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -21,7 +18,7 @@ import AddResource from './AddResource.vue';
 export default {
   components: {
     StoredResources,
-    AddResource
+    AddResource,
   },
   data() {
     return {
@@ -31,15 +28,21 @@ export default {
           id: 'official-guide',
           title: 'Official Guide',
           description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org'
+          link: 'https://vuejs.org',
         },
         {
           id: 'google',
           title: 'Google',
           description: 'Learn to google...',
-          link: 'https://google.org'
-        }
-      ]
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
     };
   },
   computed: {
@@ -48,17 +51,22 @@ export default {
     },
     addResButtonMode() {
       return this.selectedTab === 'add-resource' ? null : 'flat';
-    }
-  },
-  provide() {
-    return {
-      resources: this.storedResources
-    };
+    },
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
-    }
-  }
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+  },
 };
 </script>
